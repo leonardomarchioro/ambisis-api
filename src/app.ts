3811cc194a1c238648df1ee792514d6499f6ed1a
leonardo_marchioro <leonardomarchioro11@gmail.com>
2023-05-05 18:47:16 -0300

    feat: create middleware to validate duplicated CNPJs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "express-async-errors";
 
 import companiesRouter from "./routes/companies.routes";
+import errorHandler from "./middlewares/error-handler.middleware";
 
 const app: Express = express();
 
@@ -17,5 +18,7 @@ app.use(express.json());
 app.use("/empresa", companiesRouter);
 
 app.get('/healph-check', (req, res) => res.send({ status: "UP" }))
+
+app.use(errorHandler);
 
 export default app;
--- src/routes/companies.routes.ts
+++ src/routes/companies.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from "express";
 
+// controllers
 import createCompanyController from "../controllers/companies/create-company.controller";
 import listCompaniesController from "../controllers/companies/list-companies.controller";
 import updateCompanyController from "../controllers/companies/update-company.controller";
 import deleteCompanyController from "../controllers/companies/delete-company.controller";
 
+// middlewares
+import ValidateDuplicateCNPJMiddleware from "../middlewares/companies/validate-duplicate-cnpj.middleware";
+
 const companiesRouter = Router();
 
-companiesRouter.post("/insert", createCompanyController); 
+companiesRouter.post("/insert", ValidateDuplicateCNPJMiddleware, createCompanyController); 
 
 companiesRouter.get("", listCompaniesController); 
 
-companiesRouter.patch('/update/:id', updateCompanyController); 
+companiesRouter.patch('/update/:id', ValidateDuplicateCNPJMiddleware, updateCompanyController); 
 
 companiesRouter.delete('/delete/:id', deleteCompanyController);
 
